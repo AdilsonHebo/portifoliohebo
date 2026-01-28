d04be0043afa0a7088030eda9fac63f80d4ee9e5
AdilsonHebo <154549288+AdilsonHebo@users.noreply.github.com>
2026-01-28 08:17:23 +0000

    Add files via upload
    meu commit malanje

--- assets/js/myDocjs.js
+++ assets/js/myDocjs.js
@@ -30,13 +30,13 @@ window.addEventListener("load",()=>{
             observeAbout.observe(aboutMes)
          })
 
-         
+
 
 
     //    let btnMap=document.getElementById("btnCloseMenu")
        let menuMap=document.getElementById("menuNavegation")
 
-       
+
 
         document.querySelector("#home_pagine").addEventListener("click",()=>{
 
@@ -62,7 +62,7 @@ window.addEventListener("load",()=>{
 
 
        let boxer=document.querySelector("#ContainerCard")
-      
+
         boxer.addEventListener("click",()=>{
             let view=new IntersectionObserver((elementScrool)=>{
         window.alert(elementScrool)
@@ -78,7 +78,7 @@ window.addEventListener("load",()=>{
         var header_two=document.querySelector("#header_two")
         var titleMy=document.querySelector("#fristName")
         var btnMenuLabel=document.querySelector("#menu-drop")
-    
+
         window.addEventListener("scroll",()=>{
         let window_top=window.pageYOffset
 if(window_top>90){
@@ -86,18 +86,18 @@ if(window_top>90){
     titleMy.style.color="rgb(189, 3, 58)"
     btnMenuLabel.style.background="rgb(189, 3, 58)"
     btnMenuLabel.style.color="white"
-  
+
 }
 else{
     header_two.classList.remove("classTomenu")
     titleMy.style.color="white"
     btnMenuLabel.style.background="white"
     btnMenuLabel.style.color="rgb(189, 3, 58)"
-    
+
 }
         })
 
-       
+
 
 
         var btns =document.querySelectorAll("#btn_meu")
@@ -113,11 +113,11 @@ else{
         }
 
 
-        
+
         btns.forEach((element_btns)=>{
 
             element_btns.addEventListener("click",execute_btn)
-          
+
 
         })
 
@@ -135,21 +135,21 @@ else{
         // btnSpeak_open.addEventListener("click",()=>{
         //     modalSend.classList.add("ModalSendShow")
         // })
-       
+
 
        let myObserve=new IntersectionObserver((OlhandoElente)=>{
 
         OlhandoElente.forEach((olhando)=>{
                 if(olhando.isIntersecting){
-                    
-                    olhando.target.classList.add("classCards") 
+
+                    olhando.target.classList.add("classCards")
                 }
                 else{
-                    olhando.target.classList.remove("classCards") 
+                    olhando.target.classList.remove("classCards")
                 }
         })
        })
-              
+
        var myCards=document.querySelectorAll("#ContainerCard")
 
        myCards.forEach((observandoElemento)=>{
@@ -157,8 +157,8 @@ else{
        })
 
 
-      
-         
+
+
          let observerNetworks=new IntersectionObserver((elementsObservados)=>{
             elementsObservados.forEach((myObserversNetwork)=>{
              if(myObserversNetwork.isIntersecting==true){
@@ -171,17 +171,144 @@ else{
 
          })
 
-         let cardNetworks=document.querySelectorAll(".cardMynetwork") 
+         let cardNetworks=document.querySelectorAll(".cardMynetwork")
          cardNetworks.forEach((Observads)=>{
                 observerNetworks.observe(Observads)
                })
 
 
+let indexCarossel=0
+
+
+
+function nextCarossel(){
+    
+    let carosseImgNex=document.querySelectorAll(".linkCarossel")
+    carosseImgNex[indexCarossel].classList.remove("classShowCarossel")
+    //   indexCarossel=(indexCarossel + 1) % carosseImg.length;
+    indexCarossel ++;
+    if(indexCarossel >= carosseImgNex.length){indexCarossel=0} ;
+    carosseImgNex[indexCarossel].classList.add("classShowCarossel")
+}
+
+document.querySelector(".buttonBack").addEventListener("click",()=>{
+      nextCarossel(); 
+})
+
+
+
+// function myCarossel(){
+
+//     let carosseImg=document.querySelectorAll(".linkCarossel")
+
+
+//     carosseImg[indexCarossel].classList.remove("classShowCarossel")
+
+//     //   indexCarossel=(indexCarossel + 1) % carosseImg.length;
+//     indexCarossel ++;
+//     if(indexCarossel >= carosseImg.length){indexCarossel=0} ;
+
+//     carosseImg[indexCarossel].classList.add("classShowCarossel")
+
+// }
+
+//  setInterval(myCarossel,4000)
 
 
 
 
 
+function backCarossel(){
+    let carosseImgNex=document.querySelectorAll(".linkCarossel")
+    carosseImgNex[indexCarossel].classList.remove("classShowCarossel")
+    //   indexCarossel=(indexCarossel + 1) % carosseImg.length;
+    indexCarossel --;
+    if(indexCarossel <= 0){indexCarossel=0} ;
+    carosseImgNex[indexCarossel].classList.add("classShowCarossel")
+}
 
 
-            
+
+
+document.querySelector(".buttonNexts").addEventListener("click",()=>{
+   
+   backCarossel()
+
+})
+
+// inicio tratamento acordion
+const Accordion = {
+	  settings: {
+	    // Expand the first item by default
+	    first_expanded: false,
+	    // Allow items to be toggled independently
+	    toggle: false
+	  },
+
+	  openAccordion: function(toggle, content) {
+	    if (content.children.length) {
+	      toggle.classList.add("is-open");
+	      let final_height = Math.floor(content.children[0].offsetHeight);
+	      content.style.height = final_height + "px";
+	    }
+	  },
+
+	  closeAccordion: function(toggle, content) {
+	    toggle.classList.remove("is-open");
+	    content.style.height = 0;
+	  },
+
+	  init: function(el) {
+	    const _this = this;
+
+	    // Override default settings with classes
+	    let is_first_expanded = _this.settings.first_expanded;
+	    if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
+	    let is_toggle = _this.settings.toggle;
+	    if (el.classList.contains("is-toggle")) is_toggle = true;
+
+	    // Loop through the accordion's sections and set up the click behavior
+	    const sections = el.getElementsByClassName("accordion");
+	    const all_toggles = el.getElementsByClassName("accordion-head");
+	    const all_contents = el.getElementsByClassName("accordion-body");
+	    for (let i = 0; i < sections.length; i++) {
+	      const section = sections[i];
+	      const toggle = all_toggles[i];
+	      const content = all_contents[i];
+
+	      // Click behavior
+	      toggle.addEventListener("click", function(e) {
+	        if (!is_toggle) {
+	          // Hide all content areas first
+	          for (let a = 0; a < all_contents.length; a++) {
+	            _this.closeAccordion(all_toggles[a], all_contents[a]);
+	          }
+
+	          // Expand the clicked item
+	          _this.openAccordion(toggle, content);
+	        } else {
+	          // Toggle the clicked item
+	          if (toggle.classList.contains("is-open")) {
+	            _this.closeAccordion(toggle, content);
+	          } else {
+	            _this.openAccordion(toggle, content);
+	          }
+	        }
+	      });
+
+	      // Expand the first item
+	      if (i === 0 && is_first_expanded) {
+	        _this.openAccordion(toggle, content);
+	      }
+	    }
+	  }
+	};
+
+	(function() {
+	  // Initiate all instances on the page
+	  const accordions = document.getElementsByClassName("accordions");
+	  for (let i = 0; i < accordions.length; i++) {
+	    Accordion.init(accordions[i]);
+	  }
+	})();
+// fim tratamento acordion
